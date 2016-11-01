8d0b955cd5820218104e26141ec8f4ab8e4aa288
= <danielmonteiro@id.uff.br>
2016-11-01 10:53:34 +0000

    Forgot to add the .h for null command

---
+++ noudar-core/include/commands/CNullCommand.h
@@ -0,0 +1,21 @@
+//
+// Created by monty on 29/10/16.
+//
+
+#ifndef NOUDAR_CORE_CNULLCOMMAND_H
+#define NOUDAR_CORE_CNULLCOMMAND_H
+
+namespace Knights {
+    class CNullCommand : public IGameCommand {
+    public:
+        CNullCommand(std::shared_ptr <CGame> aGame);
+
+        virtual std::string to_string() const override;
+
+        virtual bool shouldEndTurn() override;
+
+        virtual void execute() override;
+    };
+}
+
+#endif //NOUDAR_CORE_CNULLCOMMAND_H
