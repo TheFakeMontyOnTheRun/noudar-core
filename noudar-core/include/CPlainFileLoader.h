9db9a51faa85438692e72376cddae51dfb63f2c8
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2017-03-29 15:43:40 +0100

    Allow platforms to modify the filenames before loading
    Some platforms, like DOS doesn't (for most of it's versions) allow for big filenames. This allow for transforming the filenames without changing the game code.

--- noudar-core/include/CPlainFileLoader.h
+++ noudar-core/include/CPlainFileLoader.h
@@ -7,13 +7,15 @@
 
 namespace Knights {
 	class CPlainFileLoader : public IFileLoaderDelegate {
-        std::string mPrefix = "res/";
+		std::string mPrefix = "res/";
+		std::function<std::string(std::string)> mFilenameTransformation = [](const std::string& filename) { return filename; };
 	public:
-        explicit CPlainFileLoader();
-        explicit CPlainFileLoader( std::string prefix );
+		explicit CPlainFileLoader();
+		explicit CPlainFileLoader( std::string prefix );
 		std::vector<char> loadBinaryFileFromPath( const std::string& path ) override;
 		std::string loadFileFromPath( const std::string& path ) override ;
 		std::string getFilePathPrefix() override;
+		void setFilenameTransformation( std::function<std::string(std::string)> transform );
 	};
 }
 
--- noudar-core/src/CPlainFileLoader.cpp
+++ noudar-core/src/CPlainFileLoader.cpp
@@ -5,8 +5,9 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <functional>
+#include <Logger.h>
 #include "Common.h"
-
 #include "IFileLoaderDelegate.h"
 #include "CPlainFileLoader.h"
 
@@ -21,7 +22,13 @@ namespace Knights {
     std::vector<char> CPlainFileLoader::loadBinaryFileFromPath(const std::string &path) {
         FILE *fd;
 
-        fd = fopen(( getFilePathPrefix() + path).c_str(), "rb");
+        fd = fopen((getFilePathPrefix() + mFilenameTransformation(path)).c_str(), "rb");
+
+        if (fd == nullptr) {
+            odb::Logger::log("Failed to open file: %s", path.c_str());
+            exit(0);
+        }
+
         std::vector<char> toReturn = readToBuffer(fd);
         fclose(fd);
 
@@ -31,7 +38,13 @@ namespace Knights {
     std::string CPlainFileLoader::loadFileFromPath(const std::string &path) {
         FILE *fd;
 
-        fd = fopen(( getFilePathPrefix() + path).c_str(), "r");
+        fd = fopen(( getFilePathPrefix() + mFilenameTransformation( path ) ).c_str(), "r");
+
+        if (fd == nullptr) {
+            odb::Logger::log("Failed to open file: %s", path.c_str());
+            exit(0);
+        }
+
         auto toReturn = readToString(fd);
         fclose(fd);
 
@@ -40,5 +53,9 @@ namespace Knights {
 
     std::string CPlainFileLoader::getFilePathPrefix() {
         return mPrefix;
+    }
+
+    void CPlainFileLoader::setFilenameTransformation(std::function<std::string(std::string)> transform) {
+        this->mFilenameTransformation = transform;
     }
 }
