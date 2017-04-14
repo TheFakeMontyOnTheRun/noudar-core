01466add06cd3305c7529adda909828914edeabf
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2017-04-14 19:20:13 +0100

    Simplify greatly the doorway
    Since there is reason for it to perform as a spawn point.

--- noudar-core/include/CDoorway.h
+++ noudar-core/include/CDoorway.h
@@ -1,16 +1,10 @@
 #ifndef NOUDAR_CORE_CDOORWAY_H
 #define NOUDAR_CORE_CDOORWAY_H
 namespace Knights {
-    enum EDoorwayFunction {
-        kEntry,
-        kExit
-    };
 
     class CDoorway : public IMapElement {
-        const EDoorwayFunction doorFunction;
     public:
-        CDoorway( EDoorwayFunction doorFunction );
-        const EDoorwayFunction getDoorFunction();
+        CDoorway();
     };
 }
 #endif
