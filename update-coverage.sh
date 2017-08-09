501b0e16fc577aadce7a781411d431bd898560f9
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2017-08-09 22:25:35 +0100

    Fix coverage script

--- update-coverage.sh
+++ update-coverage.sh
@@ -3,7 +3,7 @@ GMOCK_DIR="lib/googletest/googlemock"
 find . | grep gcno | xargs rm
 find . | grep gcda | xargs rm
 make clean
-make CXX='g++' CXXFLAGS='-Inoudar-core/include -Incurses-version -g -c -std=c++0x -fprofile-arcs -ftest-coverage -I$(GTEST_DIR)/include -I$(GTEST_DIR) -I$(GTEST_DIR)/include/gtest -I$(GTEST_DIR)/include/gtest/internal -I$(GMOCK_DIR)/include -I$(GMOCK_DIR) -I$(GMOCK_DIR)/include/gtest -I$(GMOCK_DIR)/include/gtest/internal' LDFLAGS='-lncurses -lpthread --coverage' unittests
+make CXX='g++' CXXFLAGS='-Inoudar-core/include -Incurses-version -g -c -std=c++0x -fprofile-arcs -ftest-coverage -I$(GTEST_DIR)/include -I$(GTEST_DIR) -I$(GTEST_DIR)/include/gtest -I$(GTEST_DIR)/include/gtest/internal -I$(GMOCK_DIR)/include -I$(GMOCK_DIR) -I$(GMOCK_DIR)/include/gtest -I$(GMOCK_DIR)/include/gtest/internal -IEASTL/include -IEASTL/test/packages/EABase/include/Common' LDFLAGS='-lncurses -lpthread --coverage' unittests
 
 ./unittests
 #gcov lib/googletest/googlemock/src/gmock_main.gcno
